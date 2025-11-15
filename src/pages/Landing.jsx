1be9fb8cbba9a2b852f6319349856c25ce505f3f
triyangupta <guptatriyan7@gmail.com>
2025-11-15 20:30:59 +0530

    update the footer

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -458,7 +458,6 @@ export default function Landing() {
         className="w-full  text-gray-100 bg-gray-700 border-t border-gray-300 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 text-center py-4 text-sm transition-colors duration-300 "
       >
         <p>© 2025 Uptoskills. Built by learners.</p>
-
       </footer>
 
       {/* Chatbot */}

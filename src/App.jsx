53a006ff42ac5c604876a1fe2ffbfb4764938b43
triyangupta <guptatriyan7@gmail.com>
2025-11-15 20:15:38 +0530

    update the footer

--- src/App.jsx
+++ src/App.jsx
@@ -68,10 +68,13 @@ function App() {
               <HeroSection />
               <AboutSection />
               <footer
-                className="w-full text-gray-100 bg-gray-700 border-t border-gray-300 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 text-center py-4 text-sm transition-colors duration-300 "
+
+                className="w-full  text-gray-100 bg-gray-700 border-t border-gray-300 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 text-center py-4 text-sm transition-colors duration-300 "
               >
                 <p>© 2025 Uptoskills. Built by learners.</p>
+
               </footer>
+              
               <Chatbot />
             </>
           } />

6a9b49976a6a263f521c14175c56db3f11c5e95e
Meghana5507 <159533179+Meghana5507@users.noreply.github.com>
2025-11-29 18:55:45 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,7 @@ import Chatbot from './components/Contact_Page/Chatbot';
 import CompanyProfilePage from './components/Company_Dashboard/companyProfilePage';
 import StudentSkillBadgesPage from "./components/Student_Dashboard/Skilledpage/StudentSkillBadgesPage";
 import Dashboard_Project from './components/Student_Dashboard/dashboard/Dashboard_Project';
-<<<<<<< HEAD
-import AboutUs from "./components/Student_Dashboard/dashboard/AboutUs";
-import MyPrograms from "./components/Student_Dashboard/dashboard/MyPrograms";
-import ForgotPassword from "./pages/ForgotPassword";
-
-// About Page Components
-=======
 // About Page components
->>>>>>> 7a437c6746659c24aaa727979aca6805923775d6
 import Header from './components/AboutPage/Header';
 import HeroSection from './components/AboutPage/HeroSection';
 import AboutSection from './components/AboutPage/AboutSection';
@@ -96,53 +88,12 @@ function App() {
           } />
           <Route path="/programs" element={<ProgramsPage />} />
           <Route path="/login" element={<LoginForm />} />
-<<<<<<< HEAD
-           <Route path="/login/forgot-password" element={<ForgotPassword />} /> 
-
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/contact" element={<ContactPage />} />
-
-          {/* ===== Dashboard Routes ===== */}
-          <Route path="/dashboard" element={<ProtectedRoute><Student_Dashboard /></ProtectedRoute>} />
-          <Route path="/dashboard/profile" element={<ProtectedRoute><UserProfilePage /></ProtectedRoute>} />
-          <Route path="/dashboard/edit-profile" element={<ProtectedRoute><EditProfilePage /></ProtectedRoute>} />
-          <Route path="/dashboard/my-programs" element={<ProtectedRoute><MyPrograms /></ProtectedRoute>} />
-          <Route path="/dashboard/notifications" element={<ProtectedRoute><NotificationsPage /></ProtectedRoute>} />
-          <Route path="/dashboard/aboutus" element={<ProtectedRoute><AboutUs /></ProtectedRoute>} />
-{/* ===== Add Project (Project Submission Form) ===== */}
-<Route path="/dashboard/add-project" element={<ProtectedRoute><MyProjects /></ProtectedRoute>} />
-
-{/* ===== My Projects (Student’s submitted projects) ===== */}
-<Route path="/dashboard/my-projects" element={<ProtectedRoute><ProjectShowcasePage /></ProtectedRoute>} />
-
-
-
-          {/* ===== Skill Badges ===== */}
-          <Route path="/mentor-dashboard/skill-badges" element={<ProtectedRoute><SkillBadgeForm /></ProtectedRoute>} />
-          <Route path="/student/skill-badges" element={<ProtectedRoute><StudentSkillBadgesPage /></ProtectedRoute>} />
-
-          {/* ===== Company Routes ===== */}
-          <Route path="/company" element={<ProtectedRoute><CompanyDashboardHome /></ProtectedRoute>} />
-          <Route path="/company-profile" element={<ProtectedRoute><CompanyProfilePage /></ProtectedRoute>} />
-          <Route path="/company/*" element={<ProtectedRoute><CompanyNotFound /></ProtectedRoute>} />
-
-          {/* ===== Misc Routes ===== */}
-          
-           <Route path="/project-showcase" element={<ProtectedRoute><Dashboard_Project view = "student" /></ProtectedRoute>} />
-          <Route path="/mentor-dashboard/project-showcase" element={<ProtectedRoute><Dashboard_Project view = "mentor" /></ProtectedRoute>} />
-          <Route path="/mentor-dashboard/*" element={<ProtectedRoute><MentorDashboardRoutes /></ProtectedRoute>} />
-          <Route path="/adminPanel" element={<ProtectedRoute><AdminPanel /></ProtectedRoute>} />
-          <Route path="/adminPanel/testimonials" element={<ProtectedRoute><AdminPanel /></ProtectedRoute>} />
-
-          {/* ===== Program Forms ===== */}
-=======
           <Route path="/login/forgot-password" element={<ForgotPassword/>}/>
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/contact" element={<ContactPage />} />
 
 
           {/* PROGRAM ROUTES */}
->>>>>>> 7a437c6746659c24aaa727979aca6805923775d6
           <Route path="/programForm/:id" element={<Webdev />} />
           {/* <Route path="/data-science" element={<Datascience />} /> */}
           <Route path="/cloud-computing" element={<Cloudcompute />} />
